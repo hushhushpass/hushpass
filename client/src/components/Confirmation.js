7ae425de7e5a1a2062c10059ba9502dd1a117c64
Andrew Patterson <apat24@mail.sfsu.edu>
2019-05-09 01:59:22 -0700

    Adds copy to clipboard button.

--- client/src/components/Confirmation.js
+++ client/src/components/Confirmation.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+// Components 
+import CopyToClipboard from './Utilities/CopyToClipboard';
 
 const Confirmation = (props) => {
 
@@ -7,6 +9,7 @@ const Confirmation = (props) => {
         <section component="Confirmation">
             <h1>Confirmation</h1>
             <Link to={`/download/${props.fileId}`}>Click here to visit the download page</Link>
+            {props.fileId && <CopyToClipboard  className="btn" text={`${window.location.href}download/${props.fileId}`}/>}
         </section>
     )
 }
