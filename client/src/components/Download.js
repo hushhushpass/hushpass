cba68ad65a2ab3a9ecfd752158a11b8228a785ce
Andrew Patterson <apat24@mail.sfsu.edu>
2019-08-21 12:03:37 -0700

    Resolves bug where valid files could not be downloaded. Removed 'fileValidity' and 'valid' key from sent object

--- client/src/components/Download.js
+++ client/src/components/Download.js
@@ -8,175 +8,186 @@ import "../styles/base.css";
 import "../styles/components/download.css";
 
 class Download extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      document: {},
-      error: ""
+    constructor(props) {
+        super(props);
+        this.state = {
+            document: {},
+            error: ""
+        };
+    }
+
+    onClickHandler = () => {
+        const data = new FormData();
+        data.append("password", document.getElementById("password").value);
+        const config = {
+            responseType: "arraybuffer",
+            headers: { "Content-Type": "multipart/form-data", accept: "" }
+        };
+
+        axios
+            .post(
+                "/api/db/file/" + this.props.match.params.fileId,
+                data,
+                config
+            )
+            .then(res => {
+                this.setState({ error: "" });
+                const url = window.URL.createObjectURL(new Blob([res.data]));
+                const link = document.createElement("a");
+                link.href = url;
+                link.download = this.state.document.fileName;
+                link.type = this.state.document.fileType;
+                document.body.appendChild(link);
+                link.click();
+            })
+            .catch(err => {
+                console.error("Error:", err.response.status);
+                const errStatus = err.response.status;
+                if (errStatus === 411) {
+                    this.setState({
+                        error:
+                            "The maximum number of downloads has been reached for this document."
+                    });
+                } else if (errStatus === 421) {
+                    this.setState({
+                        error: "The document has exceeded its expiration date."
+                    });
+                } else if (errStatus === 401) {
+                    this.setState({
+                        error: "The password entered is incorrect."
+                    });
+                }
+            });
     };
-  }
 
-  onClickHandler = () => {
-    const data = new FormData();
-    data.append("password", document.getElementById("password").value);
-    const config = {
-      responseType: "arraybuffer",
-      headers: { "Content-Type": "multipart/form-data", accept: "" }
-    };
+    componentDidMount() {
+        window.addEventListener("load", async () => {
+            const doc = await (await fetch(
+                "/api/db/" + this.props.match.params.fileId
+            )).json();
 
-    axios
-      .post("/api/db/file/" + this.props.match.params.fileId, data, config)
-      .then(res => {
-        this.setState({ error: "" });
-        const url = window.URL.createObjectURL(new Blob([res.data]));
-        const link = document.createElement("a");
-        link.href = url;
-        link.download = this.state.document.fileName;
-        link.type = this.state.document.fileType;
-        document.body.appendChild(link);
-        link.click();
-      })
-      .catch(err => {
-        console.error("Error:", err.response.status);
-        const errStatus = err.response.status;
-        if (errStatus === 411) {
-          this.setState({
-            error:
-              "The maximum number of downloads has been reached for this document."
-          });
-        } else if (errStatus === 421) {
-          this.setState({
-            error: "The document has exceeded its expiration date."
-          });
-        } else if (errStatus === 401) {
-          this.setState({
-            error: "The password entered is incorrect."
-          });
-        }
-      });
-  };
-
-  componentDidMount() {
-    window.addEventListener("load", async () => {
-      const doc = await (await fetch(
-        "/api/db/" + this.props.match.params.fileId
-      )).json();
-
-      if (doc.fileStatus === "DocLimit" || doc.fileStatus === "Expired") {
-        this.setState({
-          document: doc,
-          fileStatusMessage: "Your File is Expired"
+            if (doc.fileStatus === "DocLimit" || doc.fileStatus === "Expired") {
+                this.setState({
+                    document: doc,
+                    fileStatusMessage: "Your File is Expired"
+                });
+            } else {
+                this.setState({
+                    document: doc,
+                    fileStatusMessage: "Your File is Ready"
+                });
+            }
         });
-      } else {
-        this.setState({
-          document: doc,
-          fileStatusMessage: "Your File is Ready"
-        });
-      }
-    });
-  }
+    }
 
-  render = () => {
-    const successTemplate = (
-      <div>
-        <h1 className="center header-description header-text">
-          {this.state.fileStatusMessage}
-        </h1>
-        <ul className="flex-container">
-          <li className="flex-item">
+    render = () => {
+        const successTemplate = (
             <div>
-              <div className="note ">
-                <p className="center">{this.state.document.fileName}</p>
-                <p>Type: {this.state.document.fileType}</p>
-                <p>Id: {this.props.match.params.fileId}</p>
-                <p>Expires: {this.state.document.expirationDate}</p>
-              </div>
-              <p className="center">
-                <input
-                  type="password"
-                  id="password"
-                  name="password"
-                  placeholder="Secret Key"
-                  className="secret"
-                />
-              </p>
+                <h1 className="center header-description header-text">
+                    {this.state.fileStatusMessage}
+                </h1>
+                <ul className="flex-container">
+                    <li className="flex-item">
+                        <div>
+                            <div className="note ">
+                                <p className="center">
+                                    {this.state.document.fileName}
+                                </p>
+                                <p>Type: {this.state.document.fileType}</p>
+                                <p>Id: {this.props.match.params.fileId}</p>
+                                <p>
+                                    Expires:{" "}
+                                    {this.state.document.expirationDate}
+                                </p>
+                            </div>
+                            <p className="center">
+                                <input
+                                    type="password"
+                                    id="password"
+                                    name="password"
+                                    placeholder="Secret Key"
+                                    className="secret"
+                                />
+                            </p>
 
-              <p id="pw-error" className="alert alert-danger center">
-                <font color="red">{this.state.error}</font>
-              </p>
-              <p className="center">
-                <button
-                  type="button"
-                  className="btn"
-                  download
-                  onClick={this.onClickHandler}
-                >
-                  Download
-                </button>
-              </p>
+                            <p
+                                id="pw-error"
+                                className="alert alert-danger center"
+                            >
+                                <font color="red">{this.state.error}</font>
+                            </p>
+                            <p className="center">
+                                <button
+                                    type="button"
+                                    className="btn"
+                                    download
+                                    onClick={this.onClickHandler}
+                                >
+                                    Download
+                                </button>
+                            </p>
+                        </div>
+                    </li>
+                    <li className="flex-item">{Introduction}</li>
+                </ul>
             </div>
-          </li>
-          <li className="flex-item">{Introduction}</li>
-        </ul>
-      </div>
-    );
+        );
 
-    const expiredTemplate = (
-      <div>
-        <h1 className="center header-description header-text">
-          {/* {this.state.fileStatusMessage} */}
-          Your File is Expired or Has Reached its Download Limit
-        </h1>
-        <ul className="flex-container">
-          <li className="flex-item">
+        const expiredTemplate = (
             <div>
-              <div className="note ">
-                <p className="center">{this.state.document.fileName}</p>
-                <p>Type: {this.state.document.fileType}</p>
-                <p>Id: {this.props.match.params.fileId}</p>
-                <p>Expires: {this.state.document.expirationDate}</p>
-              </div>
-              <p className="center">
-                This file is no longer available for download
-              </p>
+                <h1 className="center header-description header-text">
+                    {/* {this.state.fileStatusMessage} */}
+                    Your File is Expired or Has Reached its Download Limit
+                </h1>
+                <ul className="flex-container">
+                    <li className="flex-item">
+                        <div>
+                            <div className="note ">
+                                <p className="center">
+                                    {this.state.document.fileName}
+                                </p>
+                                <p>Type: {this.state.document.fileType}</p>
+                                <p>Id: {this.props.match.params.fileId}</p>
+                                <p>
+                                    Expires:{" "}
+                                    {this.state.document.expirationDate}
+                                </p>
+                            </div>
+                            <p className="center">
+                                This file is no longer available for download
+                            </p>
+                        </div>
+                    </li>
+                    <li className="flex-item">{Introduction}</li>
+                </ul>
             </div>
-          </li>
-          <li className="flex-item">{Introduction}</li>
-        </ul>
-      </div>
-    );
+        );
 
-    const failTemplate = (
-      <div>
-        <div>
-          File Not Found
-          <br />
-          Id: {this.props.match.params.fileId}
-          <br />
-        </div>
-      </div>
-    );
+        const failTemplate = (
+            <div>
+                <div>
+                    File Not Found
+                    <br />
+                    Id: {this.props.match.params.fileId}
+                    <br />
+                </div>
+            </div>
+        );
 
-    const returnTemplate = () => {
-      if (this.state.document.fileValidity === undefined) {
-        return <p />;
-      } else if (
-        this.state.document.fileValidity &&
-        this.state.document.valid
-      ) {
-        return successTemplate;
-      } else if (
-        this.state.document.fileValidity &&
-        !this.state.document.valid
-      ) {
-        return expiredTemplate;
-      } else {
-        return failTemplate;
-      }
+        const returnTemplate = () => {
+            if (this.state.document.fileStatus === undefined) {
+                return <p />;
+            } else if (this.state.document.fileStatus.length === 0) {
+                return successTemplate;
+            } else if (this.state.document.fileStatus.length > 0) {
+                return expiredTemplate;
+            } else {
+                return failTemplate;
+            }
+        };
+
+        return returnTemplate();
     };
-
-    return returnTemplate();
-  };
 }
 
 export default Download;
--- routes/db.js
+++ routes/db.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const Grid = require("gridfs-stream");
 eval(
-  `Grid.prototype.findOne = ${Grid.prototype.findOne
-    .toString()
-    .replace("nextObject", "next")}`
+    `Grid.prototype.findOne = ${Grid.prototype.findOne
+        .toString()
+        .replace("nextObject", "next")}`
 );
 
 const fs = require("fs");
@@ -23,218 +23,224 @@ Grid.mongo = mongoose.mongo;
 const moment = require("moment");
 
 const getFileStatus = document => {
-  if (
-    document.maxDownloads <= document.downloadCount ||
-    document.maxDownloadsReached
-  ) {
-    return "DocLimit";
-  } else if (moment(document.expirationDate) <= moment()) {
-    return "Expired";
-  }
+    if (
+        document.maxDownloads <= document.downloadCount ||
+        document.maxDownloadsReached
+    ) {
+        return "DocLimit";
+    } else if (moment(document.expirationDate) <= moment()) {
+        return "Expired";
+    }
 
-  return "";
+    return "";
 };
 
 router.post("/upload", function(req, res) {
-  const form = new formidable.IncomingForm();
-  form.parse(req, async function(err, fields, files) {
-    if (files.file.size > 24500000) {
-      console.log("File is too large");
-      // Sending a proper 401 or 500 status crashes the backend.
-      res.status(200).send("File is too large");
-    }
+    const form = new formidable.IncomingForm();
+    form.parse(req, async function(err, fields, files) {
+        if (files.file.size > 24500000) {
+            console.log("File is too large");
+            // Sending a proper 401 or 500 status crashes the backend.
+            res.status(200).send("File is too large");
+        }
 
-    const docId = uuidv4();
-    const newDoc = await new Document({
-      docId: docId,
-      fileName: files["file"].name,
-      fileType: files["file"].type,
-      hashedKey: crypto
-        .createHash("sha256")
-        .update(process.env.SALT + fields.key)
-        .digest("hex"),
-      userID: req.userID,
-      maxDownloads: fields.downloads ? fields.downloads : 1,
-      expirationDate: fields.expiration
-        ? moment().add(fields.expiration, "d")
-        : moment().add(1, "d")
-    }).save();
+        const docId = uuidv4();
+        const newDoc = await new Document({
+            docId: docId,
+            fileName: files["file"].name,
+            fileType: files["file"].type,
+            hashedKey: crypto
+                .createHash("sha256")
+                .update(process.env.SALT + fields.key)
+                .digest("hex"),
+            userID: req.userID,
+            maxDownloads: fields.downloads ? fields.downloads : 1,
+            expirationDate: fields.expiration
+                ? moment().add(fields.expiration, "d")
+                : moment().add(1, "d")
+        }).save();
 
-    let realKey = crypto.scryptSync(fields.key, process.env.SALT, 32);
-    const cipher = crypto.createCipheriv(
-      "aes-256-cbc",
-      realKey,
-      Buffer.from(process.env.SALT, "ascii")
-        .toString("hex")
-        .slice(0, 16)
-    );
-    const input = fs.createReadStream(files["file"].path);
+        let realKey = crypto.scryptSync(fields.key, process.env.SALT, 32);
+        const cipher = crypto.createCipheriv(
+            "aes-256-cbc",
+            realKey,
+            Buffer.from(process.env.SALT, "ascii")
+                .toString("hex")
+                .slice(0, 16)
+        );
+        const input = fs.createReadStream(files["file"].path);
 
-    const encryptedFilePath = files["file"].path + ".enc";
-    const output = fs.createWriteStream(encryptedFilePath);
+        const encryptedFilePath = files["file"].path + ".enc";
+        const output = fs.createWriteStream(encryptedFilePath);
 
-    input.pipe(cipher).pipe(output);
+        input.pipe(cipher).pipe(output);
 
-    //FILE HAS BEEN SUCCESSFULLY ENCRYPTED
-    output.on("finish", function() {
-      // console.log("Encrypted file written to disk!");
-      const gridfs = Grid(connection.db, mongoose.mongo);
+        //FILE HAS BEEN SUCCESSFULLY ENCRYPTED
+        output.on("finish", function() {
+            // console.log("Encrypted file written to disk!");
+            const gridfs = Grid(connection.db, mongoose.mongo);
 
-      const writestream = gridfs.createWriteStream({
-        filename: docId
-      });
+            const writestream = gridfs.createWriteStream({
+                filename: docId
+            });
 
-      fs.createReadStream(encryptedFilePath).pipe(writestream);
+            fs.createReadStream(encryptedFilePath).pipe(writestream);
 
-      writestream.on("close", function(file) {
-        console.log("file added to db");
-      });
-      writestream.on("error", function(err) {
-        console.error("error");
-        res.status(200).send("Could not add file to db");
-      });
+            writestream.on("close", function(file) {
+                console.log("file added to db");
+            });
+            writestream.on("error", function(err) {
+                console.error("error");
+                res.status(200).send("Could not add file to db");
+            });
 
-      return res.status(200).send(docId);
+            return res.status(200).send(docId);
+        });
     });
-  });
 });
 
 router.get("/:documentCode", async function(req, res) {
-  const docId = req.params.documentCode;
+    const docId = req.params.documentCode;
 
-  const document = await Document.findOne({ docId: docId }, function(err, doc) {
-    if (err) {
-      console.trace(err);
-    }
-  });
-
-  if (document === null) {
-    res.status(404).send({
-      fileName: "",
-      fileType: "",
-      expirationDate: "",
-      fileStatus: "",
-      fileValidity: false
+    const document = await Document.findOne({ docId: docId }, function(
+        err,
+        doc
+    ) {
+        if (err) {
+            console.trace(err);
+        }
     });
-    res.end();
-  }
 
-  const fileStatus = getFileStatus(document);
+    if (document === null) {
+        res.status(404).send({
+            fileName: "",
+            fileType: "",
+            expirationDate: "",
+            fileStatus: ""
+        });
+        res.end();
+    }
 
-  res.status(200).send({
-    fileName: document.fileName,
-    fileType: document.fileType,
-    expirationDate: moment(document.expirationDate).format(
-      "dddd, MMMM Do YYYY"
-    ),
-    fileStatus,
-    fileValidity: true
-  });
+    const fileStatus = getFileStatus(document);
+
+    res.status(200).send({
+        fileName: document.fileName,
+        fileType: document.fileType,
+        expirationDate: moment(document.expirationDate).format(
+            "dddd, MMMM Do YYYY"
+        ),
+        fileStatus
+    });
 });
 
 router.post("/file/:documentCode", async function(req, res) {
-  const docId = req.params.documentCode;
+    const docId = req.params.documentCode;
 
-  const form = new formidable.IncomingForm();
+    const form = new formidable.IncomingForm();
 
-  form.parse(req, async function(err, fields) {
-    const document = await Document.findOne({ docId: docId }, function(
-      err,
-      doc
-    ) {
-      if (err) return console.error(err);
-    });
-    const docStatus = getFileStatus(document);
-
-    if (docStatus === "Expired") {
-      return res.status(421).send({
-        error: "Document has been expired."
-      });
-    }
-
-    if (docStatus === "DocLimit") {
-      if (!document.maxDownloadsReached) {
-        Document.findOneAndUpdate(
-          {
-            docId
-          },
-          {
-            $set: {
-              maxDownloadsReached: true
-            }
-          },
-          { new: true },
-          result => {
-            // console.log(result);
-          }
-        );
-      }
-      return res.status(411).send({
-        error:
-          "The maximum number of downloads has been reached for this document."
-      });
-    } else {
-    }
-
-    const hash = crypto
-      .createHash("sha256")
-      .update(process.env.SALT + fields.password)
-      .digest("hex");
-
-    if (hash != document.hashedKey) return res.status(401).send("Bad password");
-
-    const gridfs = await Grid(connection.db, mongoose.mongo);
-
-    gridfs.findOne({ filename: docId }, function(err, file) {
-      if (err) return res.status(400).send(err);
-      else if (!file) {
-        return res.status(404).send({
-          fileValidity: false
+    form.parse(req, async function(err, fields) {
+        const document = await Document.findOne({ docId: docId }, function(
+            err,
+            doc
+        ) {
+            if (err) return console.error(err);
         });
-      }
+        const docStatus = getFileStatus(document);
 
-      res.set("Content-Type", document.fileType);
-      res.set(
-        "Content-Disposition",
-        'attachment; filename="' + document.fileName + '"'
-      );
+        if (docStatus === "Expired") {
+            return res.status(421).send({
+                error: "Document has been expired."
+            });
+        }
 
-      let realKey = crypto.scryptSync(fields.password, process.env.SALT, 32);
-      const cipher = crypto.createDecipheriv(
-        "aes-256-cbc",
-        realKey,
-        Buffer.from(process.env.SALT, "ascii")
-          .toString("hex")
-          .slice(0, 16)
-      );
-      const readstream = gridfs.createReadStream({ filename: docId });
-      readstream.pipe(cipher).pipe(res);
-
-      readstream.on("end", function() {
-        Document.findOneAndUpdate(
-          {
-            docId
-          },
-          {
-            $set: {
-              downloadCount: document.downloadCount + 1
+        if (docStatus === "DocLimit") {
+            if (!document.maxDownloadsReached) {
+                Document.findOneAndUpdate(
+                    {
+                        docId
+                    },
+                    {
+                        $set: {
+                            maxDownloadsReached: true
+                        }
+                    },
+                    { new: true },
+                    result => {
+                        // console.log(result);
+                    }
+                );
             }
-          },
-          { new: true },
-          result => {
-            // console.log(result);
-          }
-        );
+            return res.status(411).send({
+                error:
+                    "The maximum number of downloads has been reached for this document."
+            });
+        } else {
+        }
 
-        res.end();
-      });
+        const hash = crypto
+            .createHash("sha256")
+            .update(process.env.SALT + fields.password)
+            .digest("hex");
 
-      readstream.on("error", function(err) {
-        console.error("error");
-        res.end();
-      });
+        if (hash != document.hashedKey)
+            return res.status(401).send("Bad password");
+
+        const gridfs = await Grid(connection.db, mongoose.mongo);
+
+        gridfs.findOne({ filename: docId }, function(err, file) {
+            if (err) return res.status(400).send(err);
+            else if (!file) {
+                return res.status(404).send({
+                    fileValidity: false
+                });
+            }
+
+            res.set("Content-Type", document.fileType);
+            res.set(
+                "Content-Disposition",
+                'attachment; filename="' + document.fileName + '"'
+            );
+
+            let realKey = crypto.scryptSync(
+                fields.password,
+                process.env.SALT,
+                32
+            );
+            const cipher = crypto.createDecipheriv(
+                "aes-256-cbc",
+                realKey,
+                Buffer.from(process.env.SALT, "ascii")
+                    .toString("hex")
+                    .slice(0, 16)
+            );
+            const readstream = gridfs.createReadStream({ filename: docId });
+            readstream.pipe(cipher).pipe(res);
+
+            readstream.on("end", function() {
+                Document.findOneAndUpdate(
+                    {
+                        docId
+                    },
+                    {
+                        $set: {
+                            downloadCount: document.downloadCount + 1
+                        }
+                    },
+                    { new: true },
+                    result => {
+                        // console.log(result);
+                    }
+                );
+
+                res.end();
+            });
+
+            readstream.on("error", function(err) {
+                console.error("error");
+                res.end();
+            });
+        });
     });
-  });
 });
 
 module.exports = router;
