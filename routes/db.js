934905ee6d265de669d3d87dca80c235e174220b
Peter M <pmalolep@mail.sfsu.edu>
2019-08-22 20:26:32 -0700

    moved db functions to seperate file. also updated a few things to stop those annoying deprication warnings

--- routes/db.js
+++ routes/db.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const mdb = require("../services/MongoDB/index.js");
 
 const mongoose = require("mongoose");
 const Grid = require("gridfs-stream");
 eval(
-  `Grid.prototype.findOne = ${Grid.prototype.findOne
+    `Grid.prototype.findOne = ${Grid.prototype.findOne
     .toString()
     .replace("nextObject", "next")}`
 );
@@ -23,218 +24,181 @@ Grid.mongo = mongoose.mongo;
 const moment = require("moment");
 
 const getFileStatus = document => {
-  if (
-    document.maxDownloads <= document.downloadCount ||
-    document.maxDownloadsReached
-  ) {
-    return "DocLimit";
-  } else if (moment(document.expirationDate) <= moment()) {
-    return "Expired";
-  }
+    if (
+        document.maxDownloads <= document.downloadCount
+        //|| document.maxDownloadsReached
+    ) {
+        return "DocLimit";
+    } else if (moment(document.expirationDate) <= moment()) {
+        return "Expired";
+    }
 
-  return "";
+    return "";
 };
 
 router.post("/upload", function(req, res) {
-  const form = new formidable.IncomingForm();
-  form.parse(req, async function(err, fields, files) {
-    if (files.file.size > 24500000) {
-      console.log("File is too large");
-      // Sending a proper 401 or 500 status crashes the backend.
-      res.status(200).send("File is too large");
-    }
+    const form = new formidable.IncomingForm();
+    form.parse(req, async function(err, fields, files) {
+        if (files.file.size > 24500000) {
+            console.log("File is too large");
+            // Sending a proper 401 or 500 status crashes the backend.
+            res.status(200).send("File is too large");
+        }
 
-    const docId = uuidv4();
-    const newDoc = await new Document({
-      docId: docId,
-      fileName: files["file"].name,
-      fileType: files["file"].type,
-      hashedKey: crypto
-        .createHash("sha256")
-        .update(process.env.SALT + fields.key)
-        .digest("hex"),
-      userID: req.userID,
-      maxDownloads: fields.downloads ? fields.downloads : 1,
-      expirationDate: fields.expiration
-        ? moment().add(fields.expiration, "d")
-        : moment().add(1, "d")
-    }).save();
+        const docId = await mdb.saveNewDocumentToDB(
+            files["file"].name,
+            files["file"].type,
+            fields.key,
+            fields.downloads,
+            fields.expiration
+        );
+        // const docId = uuidv4();
+        // const newDoc = await new Document({
+        //   docId: docId,
+        //   fileName: files["file"].name,
+        //   fileType: files["file"].type,
+        //   hashedKey: crypto
+        //     .createHash("sha256")
+        //     .update(process.env.SALT + fields.key)
+        //     .digest("hex"),
+        //   userID: req.userID,
+        //   maxDownloads: fields.downloads ? fields.downloads : 1,
+        //   expirationDate: fields.expiration
+        //     ? moment().add(fields.expiration, "d")
+        //     : moment().add(1, "d")
+        // }).save();
 
-    let realKey = crypto.scryptSync(fields.key, process.env.SALT, 32);
-    const cipher = crypto.createCipheriv(
-      "aes-256-cbc",
-      realKey,
-      Buffer.from(process.env.SALT, "ascii")
-        .toString("hex")
-        .slice(0, 16)
-    );
-    const input = fs.createReadStream(files["file"].path);
+        let realKey = crypto.scryptSync(fields.key, process.env.SALT, 32);
+        const cipher = crypto.createCipheriv(
+            "aes-256-cbc",
+            realKey,
+            Buffer.from(process.env.SALT, "ascii")
+            .toString("hex")
+            .slice(0, 16)
+        );
+        const input = fs.createReadStream(files["file"].path);
 
-    const encryptedFilePath = files["file"].path + ".enc";
-    const output = fs.createWriteStream(encryptedFilePath);
+        const encryptedFilePath = files["file"].path + ".enc";
+        const output = fs.createWriteStream(encryptedFilePath);
 
-    input.pipe(cipher).pipe(output);
+        input.pipe(cipher).pipe(output);
 
-    //FILE HAS BEEN SUCCESSFULLY ENCRYPTED
-    output.on("finish", function() {
-      // console.log("Encrypted file written to disk!");
-      const gridfs = Grid(connection.db, mongoose.mongo);
-
-      const writestream = gridfs.createWriteStream({
-        filename: docId
-      });
-
-      fs.createReadStream(encryptedFilePath).pipe(writestream);
-
-      writestream.on("close", function(file) {
-        console.log("file added to db");
-      });
-      writestream.on("error", function(err) {
-        console.error("error");
-        res.status(200).send("Could not add file to db");
-      });
-
-      return res.status(200).send(docId);
+        //FILE HAS BEEN SUCCESSFULLY ENCRYPTED
+        output.on("finish", function() {
+            const status = mdb.saveEncryptedFileToDB(
+                docId,
+                fs.createReadStream(encryptedFilePath)
+            );
+            if (status) {
+                res.status(200).send(docId);
+                // console.log("file added to db");
+            } else {
+                res.status(200).send("Could not add file to db");
+                console.log("error, failed to upload file");
+            }
+        });
     });
-  });
 });
 
 router.get("/:documentCode", async function(req, res) {
-  const docId = req.params.documentCode;
+    const docId = req.params.documentCode;
 
-  const document = await Document.findOne({ docId: docId }, function(err, doc) {
-    if (err) {
-      console.trace(err);
-    }
-  });
-
-  if (document === null) {
-    res.status(404).send({
-      fileName: "",
-      fileType: "",
-      expirationDate: "",
-      fileStatus: "",
-      fileValidity: false
+    const document = await Document.findOne({ docId: docId }, function(err, doc) {
+        if (err) {
+            console.trace(err);
+        }
     });
-    res.end();
-  }
 
-  const fileStatus = getFileStatus(document);
+    if (document === null) {
+        res.status(404).send({
+            fileName: "",
+            fileType: "",
+            expirationDate: "",
+            fileStatus: "",
+            fileValidity: false
+        });
 
-  res.status(200).send({
-    fileName: document.fileName,
-    fileType: document.fileType,
-    expirationDate: moment(document.expirationDate).format(
-      "dddd, MMMM Do YYYY"
-    ),
-    fileStatus,
-    fileValidity: true
-  });
+        res.end();
+    }
+
+    const fileStatus = getFileStatus(document);
+
+    res.status(200).send({
+        fileName: document.fileName,
+        fileType: document.fileType,
+        expirationDate: moment(document.expirationDate).format(
+            "dddd, MMMM Do YYYY"
+        ),
+        fileStatus,
+        valid: document.valid,
+        fileValidity: true
+    });
 });
 
 router.post("/file/:documentCode", async function(req, res) {
-  const docId = req.params.documentCode;
+    const docId = req.params.documentCode;
 
-  const form = new formidable.IncomingForm();
+    const form = new formidable.IncomingForm();
 
-  form.parse(req, async function(err, fields) {
-    const document = await Document.findOne({ docId: docId }, function(
-      err,
-      doc
-    ) {
-      if (err) return console.error(err);
-    });
-    const docStatus = getFileStatus(document);
-
-    if (docStatus === "Expired") {
-      return res.status(421).send({
-        error: "Document has been expired."
-      });
-    }
-
-    if (docStatus === "DocLimit") {
-      if (!document.maxDownloadsReached) {
-        Document.findOneAndUpdate(
-          {
-            docId
-          },
-          {
-            $set: {
-              maxDownloadsReached: true
-            }
-          },
-          { new: true },
-          result => {
-            // console.log(result);
-          }
-        );
-      }
-      return res.status(411).send({
-        error:
-          "The maximum number of downloads has been reached for this document."
-      });
-    } else {
-    }
-
-    const hash = crypto
-      .createHash("sha256")
-      .update(process.env.SALT + fields.password)
-      .digest("hex");
-
-    if (hash != document.hashedKey) return res.status(401).send("Bad password");
-
-    const gridfs = await Grid(connection.db, mongoose.mongo);
-
-    gridfs.findOne({ filename: docId }, function(err, file) {
-      if (err) return res.status(400).send(err);
-      else if (!file) {
-        return res.status(404).send({
-          fileValidity: false
+    form.parse(req, async function(err, fields) {
+        const document = await Document.findOne({ docId: docId }, function(
+            err,
+            doc
+        ) {
+            if (err) return console.error(err);
         });
-      }
+        const docStatus = getFileStatus(document);
 
-      res.set("Content-Type", document.fileType);
-      res.set(
-        "Content-Disposition",
-        'attachment; filename="' + document.fileName + '"'
-      );
+        if (docStatus === "Expired") {
+            return res.status(421).send({
+                error: "The document has expired."
+            });
+        }
 
-      let realKey = crypto.scryptSync(fields.password, process.env.SALT, 32);
-      const cipher = crypto.createDecipheriv(
-        "aes-256-cbc",
-        realKey,
-        Buffer.from(process.env.SALT, "ascii")
-          .toString("hex")
-          .slice(0, 16)
-      );
-      const readstream = gridfs.createReadStream({ filename: docId });
-      readstream.pipe(cipher).pipe(res);
+        if (docStatus === "DocLimit") {
+            return res.status(411).send({
+                error: "The maximum number of downloads has been reached for this document."
+            });
+        }
 
-      readstream.on("end", function() {
-        Document.findOneAndUpdate(
-          {
-            docId
-          },
-          {
-            $set: {
-              downloadCount: document.downloadCount + 1
-            }
-          },
-          { new: true },
-          result => {
-            // console.log(result);
-          }
+        const hash = crypto
+            .createHash("sha256")
+            .update(process.env.SALT + fields.password)
+            .digest("hex");
+
+        if (hash != document.hashedKey) return res.status(401).send("Bad password");
+
+        res.set("Content-Type", document.fileType);
+        res.set(
+            "Content-Disposition",
+            'attachment; filename="' + document.fileName + '"'
         );
 
-        res.end();
-      });
+        let realKey = crypto.scryptSync(fields.password, process.env.SALT, 32);
+        const cipher = crypto.createDecipheriv(
+            "aes-256-cbc",
+            realKey,
+            Buffer.from(process.env.SALT, "ascii")
+            .toString("hex")
+            .slice(0, 16)
+        );
+        const encryptedReadStream = await mdb.readStreamEncryptedFileFromDB(docId);
 
-      readstream.on("error", function(err) {
-        console.error("error");
-        res.end();
-      });
+        await encryptedReadStream.pipe(cipher).pipe(res);
+
+        encryptedReadStream.once("end", function() {
+            mdb.updateDocumentAfterDownload(docId);
+            // res.end(); // causes error, res ends automatically
+        });
+
+        encryptedReadStream.once("error", function(err) {
+            console.error("Error durring encryptedReadStream:", err);
+            return res.status(400).send({
+                fileValidity: false,
+                error: "An error occured or the encrypted file could not be found"
+            });
+        });
     });
-  });
 });
 
 module.exports = router;
---
+++ services/MongoDB/index.js
@@ -0,0 +1,104 @@
+const mongoose = require("mongoose");
+mongoose.set("useCreateIndex", true);
+mongoose.set("useFindAndModify", false);
+const Grid = require("gridfs-stream");
+eval(
+    `Grid.prototype.findOne = ${Grid.prototype.findOne
+    .toString()
+    .replace("nextObject", "next")}`
+);
+
+const uuidv4 = require("uuid/v4");
+const Document = mongoose.model("documents");
+const crypto = require("crypto");
+
+mongoose.Promise = global.Promise;
+const connection = mongoose.connection;
+Grid.mongo = mongoose.mongo;
+
+const moment = require("moment");
+
+async function saveNewDocumentToDB(name, type, key, downloads, expiration) {
+    const docId = uuidv4();
+    await new Document({
+        docId: docId,
+        fileName: name,
+        fileType: type,
+        hashedKey: crypto
+            .createHash("sha256")
+            .update(process.env.SALT + key)
+            .digest("hex"),
+        maxDownloads: downloads ? downloads : 1,
+        expirationDate: expiration ?
+            moment().add(expiration, "d") :
+            moment().add(1, "d")
+    }).save();
+
+    return docId;
+}
+
+async function saveEncryptedFileToDB(docId, encryptedFileReadStream) {
+    const gridFSBucket = await new mongoose.mongo.GridFSBucket(connection.db);
+    const writestream = gridFSBucket.openUploadStream(docId, function(err) {
+        if (err) {
+            console.log(
+                "Error in saveEncryptedFileToDB: unable to establish writestream"
+            );
+            return -1;
+        }
+    });
+
+    encryptedFileReadStream.pipe(writestream);
+
+    writestream.on("close", function(file) {
+        console.log("file added to db");
+        return 1;
+    });
+    writestream.on("error", function(err) {
+        console.error("error");
+        return -1;
+    });
+}
+
+async function updateDocumentAfterDownload(docId) {
+    Document.findOneAndUpdate({ docId: docId }, {
+            $inc: { downloadCount: 1 }
+        }, { new: true },
+        function(err, response) {
+            if (err) {
+                console.log(err);
+            } else {
+                // console.log(response);
+            }
+        }
+    );
+}
+
+async function getDocument(docId) {
+    return Document.findOne({ docId }, function(err) {
+        if (err) {
+            console.trace(err);
+        }
+    });
+}
+
+async function readStreamEncryptedFileFromDB(docId) {
+    const gridFSBucket = await new mongoose.mongo.GridFSBucket(connection.db);
+    const readStream = gridFSBucket.openDownloadStreamByName(docId, function(
+        err
+    ) {
+        if (err) {
+            console.log("error in readStreamEncryptedFileFromDB");
+            return 0;
+        }
+    });
+    return readStream;
+}
+
+module.exports = {
+    saveNewDocumentToDB,
+    saveEncryptedFileToDB,
+    updateDocumentAfterDownload,
+    getDocument,
+    readStreamEncryptedFileFromDB
+};
